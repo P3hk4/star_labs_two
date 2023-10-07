8842aea7af3b4c3d3d4886d94773f523b67b6ed0
Pavel Tarasov <pavel44tarasov@yandex.ru>
2023-10-08 00:49:15 +0400

    ConstantFunction class editing commit

--- src/main/java/functions/ConstantFunction.java
+++ src/main/java/functions/ConstantFunction.java
@@ -2,7 +2,17 @@ package functions;
 
 public class ConstantFunction implements MathFunction {
 
+    private final double constant;
+
     public double apply(double x) {
-        return 0;
+        return this.constant;
+    }
+
+    public ConstantFunction(double constant) {
+        this.constant = constant;
+    }
+
+    public double getConstant(){
+        return this.constant;
     }
 }
