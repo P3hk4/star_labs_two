0c2229815c0befa4411f0b4d105c1ddb18cb6abc
Pavel Tarasov <pavel44tarasov@yandex.ru>
2023-10-08 21:52:47 +0400

    ArrayTabulatedFunctionTests creating commit

---
+++ src/test/java/functions/ArrayTabulatedFunctionTest.java
@@ -0,0 +1,128 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayTabulatedFunctionTest {
+
+//    for (int i = 0; i < ATF.getCount(); ++i){
+//        System.out.println(ATF.getX(i) +  "  -  " + ATF.getY(i));
+//    }
+
+    @Test
+    void floorIndexOfX() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,100,100);
+        assertEquals(15,ATF.floorIndexOfX(16.5));
+        assertEquals(99,ATF.floorIndexOfX(110));
+        assertEquals(0,ATF.floorIndexOfX(-5));
+
+    }
+
+    @Test
+    void extrapolateLeft() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 10,20,11);
+        assertEquals(79,ATF.extrapolateLeft(9));
+        assertEquals(58,ATF.extrapolateLeft(8));
+    }
+
+    @Test
+    void extrapolateRight() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,10,10);
+            for (int i = 0; i < ATF.getCount(); ++i){
+        System.out.println(ATF.getX(i) +  "  -  " + ATF.getY(i));
+    }
+        assertEquals(119,ATF.extrapolateRight(11));
+        assertEquals(138,ATF.extrapolateRight(12));
+    }
+
+    @Test
+    void interpolate() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,10,10);
+        for (int i = 0; i < ATF.getCount(); ++i){
+            System.out.println(ATF.getX(i) +  "  -  " + ATF.getY(i));
+        }
+        assertEquals(30.5,ATF.interpolate(5.5,ATF.floorIndexOfX(5.5)));
+        assertEquals(72.5,ATF.interpolate(8.5,ATF.floorIndexOfX(8.5)));
+        assertEquals(ATF.interpolate(5.5,ATF.floorIndexOfX(5.5)),ATF.interpolate(5.5,ATF.getX(4),ATF.getX(5),ATF.getY(4),ATF.getY(5)));
+        assertEquals(ATF.interpolate(8.5,ATF.floorIndexOfX(8.5)),ATF.interpolate(8.5,ATF.getX(7),ATF.getX(8),ATF.getY(7),ATF.getY(8)));
+
+    }
+
+    @Test
+    void getCount() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,10,10);
+        assertEquals(10,ATF.getCount());
+    }
+
+    @Test
+    void getX() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,10,10);
+        assertEquals(5,ATF.getX(4));
+        assertEquals(8,ATF.getX(7));
+        assertEquals(2,ATF.getX(1));
+    }
+
+    @Test
+    void getY() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,10,10);
+        assertEquals(25,ATF.getY(4));
+        assertEquals(64,ATF.getY(7));
+        assertEquals(4,ATF.getY(1));
+    }
+
+    @Test
+    void setY() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,10,10);
+        assertEquals(25,ATF.getY(4));
+        ATF.setY(4,36);
+        assertEquals(36,ATF.getY(4));
+    }
+
+    @Test
+    void indexOfX() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,10,10);
+        assertEquals(1,ATF.indexOfX(2));
+        assertEquals(6,ATF.indexOfX(7));
+    }
+
+    @Test
+    void indexOfY() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,10,10);
+        assertEquals(1,ATF.indexOfY(4));
+        assertEquals(6,ATF.indexOfY(49));
+    }
+
+    @Test
+    void leftBound() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,10,10);
+        assertEquals(ATF.xValues[0],ATF.leftBound());
+    }
+
+    @Test
+    void rightBound() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 1,10,10);
+        assertEquals(ATF.xValues[ATF.getCount()-1],ATF.rightBound());
+    }
+
+    @Test
+    void apply() {
+        SqrFunction sqrFunction = new SqrFunction();
+        ArrayTabulatedFunction ATF = new ArrayTabulatedFunction(sqrFunction, 10,100,90);
+        System.out.println(ATF.apply(101));
+        System.out.println(ATF.apply(9));
+        System.out.println(ATF.apply(45.5));
+    }
+}
