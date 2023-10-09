fe877100e2b761205dc5aada55079d6b33f82e9c
Pavel Tarasov <pavel44tarasov@yandex.ru>
2023-10-09 13:25:24 +0400

    LogMathFunctionTest editing commit

--- src/test/java/functions/LogMathFunctionTest.java
+++ src/test/java/functions/LogMathFunctionTest.java
@@ -10,7 +10,7 @@ class LogMathFunctionTest {
     @Test
     void apply() {
         LogMathFunction logMathFunction = new LogMathFunction();
-        assertEquals(2.302585092994046D, logMathFunction.apply(10));
+        assertEquals(2.3, logMathFunction.apply(10), 2.3*0.05);
         assertEquals(1, logMathFunction.apply(Math.E));
         assertEquals(NaN,logMathFunction.apply(-10));
     }
