fdb7bcc78ce44506f77631ec1717e4e7de948fe6
Pavel Tarasov <pavel44tarasov@yandex.ru>
2023-10-23 10:17:48 +0400

    IdentityFunction editing commit

--- src/main/java/functions/IdentityFunction.java
+++ src/main/java/functions/IdentityFunction.java
@@ -1,8 +1,28 @@
 package functions;
 
-public class IdentityFunction implements MathFunction{
+public class IdentityFunction implements MathFunction, Cloneable{
     public double apply(double x) {
         return x;
     }
 
+    @Override
+    public String toString() {
+        return "IdentityFunction - Описывает математическую функцию, заданную при создании экземпляра класса. " +
+                "На вход принимает исходное значение Double, возвращает значение Double после приминения описанной функции.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof IdentityFunction;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
