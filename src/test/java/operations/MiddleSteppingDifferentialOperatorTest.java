fd4846c34ece5f5fcc8aa82cc6e4b059f8ce5e1b
Pavel Tarasov <pavel44tarasov@yandex.ru>
2023-11-06 19:29:29 +0400

    Operators tests creating commit

--- src/test/java/operations/MiddleSteppingDifferentialOperatorTest.java
+++ src/test/java/operations/MiddleSteppingDifferentialOperatorTest.java
@@ -1,5 +1,7 @@
 package operations;
 
+import functions.MathFunction;
+import functions.SqrFunction;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,5 +10,10 @@ class MiddleSteppingDifferentialOperatorTest {
 
     @Test
     void derive() {
+        SteppingDifferentialOperator middleSteppingDifferentialOperator = new MiddleSteppingDifferentialOperator(1);
+        SqrFunction sqrFunction = new SqrFunction();
+        MathFunction deferentialSqrFunction = middleSteppingDifferentialOperator.derive(sqrFunction);
+        assertEquals(24, deferentialSqrFunction.apply(12), 1);
+        assertEquals(40, deferentialSqrFunction.apply(20), 1);
     }
 }
--- src/test/java/operations/RightSteppingDifferentialOperatorTest.java
+++ src/test/java/operations/RightSteppingDifferentialOperatorTest.java
@@ -1,5 +1,7 @@
 package operations;
 
+import functions.MathFunction;
+import functions.SqrFunction;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,5 +10,10 @@ class RightSteppingDifferentialOperatorTest {
 
     @Test
     void derive() {
+        SteppingDifferentialOperator rightSteppingDifferentialOperator = new RightSteppingDifferentialOperator(1);
+        SqrFunction sqrFunction = new SqrFunction();
+        MathFunction deferentialSqrFunction = rightSteppingDifferentialOperator.derive(sqrFunction);
+        assertEquals(14, deferentialSqrFunction.apply(7), 1);
+        assertEquals(16, deferentialSqrFunction.apply(8), 1);
     }
 }
