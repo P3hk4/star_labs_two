9579face057aae41071f9ee226a0efec0e158468
mitia <mitia.schepetov@yandex.ru>
2023-11-19 15:55:17 +0400

    writeTabulatedFunction

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -2,9 +2,8 @@ package io;
 
 import functions.Point;
 import functions.TabulatedFunction;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import java.io.*;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -22,6 +21,16 @@ public final class FunctionsIO {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dos = new DataOutputStream(outputStream);
+        dos.writeInt(function.getCount());
+        for (Point point : function) {
+            dos.writeDouble(point.x);
+            dos.writeDouble(point.y);
+        }
+        dos.flush();
     }
 
 }
