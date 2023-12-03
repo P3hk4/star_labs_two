a79537007c5f35c71f22a1519165a234afb90aca
Pavel Tarasov <pavel44tarasov@yandex.ru>
2023-12-03 23:25:53 +0400

    MultiplyingTask commit

---
+++ src/main/java/concurrent/MultiplyingTask.java
@@ -0,0 +1,20 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable {
+
+    private TabulatedFunction tabulatedFunction;
+
+    public MultiplyingTask(TabulatedFunction tabulatedFunction) {
+        this.tabulatedFunction = tabulatedFunction;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i<tabulatedFunction.getCount(); i++){
+            tabulatedFunction.setY(i,2*tabulatedFunction.getY(i));
+        }
+        System.out.println(Thread.currentThread().getName() + " закончил работу!");
+    }
+}
