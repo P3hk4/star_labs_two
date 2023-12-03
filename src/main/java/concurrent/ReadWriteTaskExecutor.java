df4dcdac2f391589984f4c4ddd9e9b82dc37c72b
mitia <mitia.schepetov@yandex.ru>
2023-12-03 18:50:00 +0400

    iterator() у SynchronizedTabulatedFunction

--- src/main/java/concurrent/ReadWriteTaskExecutor.java
+++ src/main/java/concurrent/ReadWriteTaskExecutor.java
@@ -8,10 +8,13 @@ public class ReadWriteTaskExecutor {
     public static void main(String[] args) {
         ConstantFunction sourse = new ConstantFunction(-1);
         TabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(sourse, 1, 1000, 1000);
+
         ReadTask readTask = new ReadTask(tabulatedFunction);
         Thread threadRead = new Thread(readTask);
+
         WriteTask writeTask = new WriteTask(tabulatedFunction, 0.5);
         Thread threadWrite = new Thread(writeTask);
+
         threadRead.start();
         threadWrite.start();
     }
