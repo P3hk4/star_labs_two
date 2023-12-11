f7fe0acf45e8163fd9bbe018d9e11c54d4a1e00d
Pavel Tarasov <pavel44tarasov@yandex.ru>
2023-12-12 03:17:48 +0400

    WebController update commit

--- out/artifacts/Lab_2_war_exploded/ui/WebController.java
+++ out/artifacts/Lab_2_war_exploded/ui/WebController.java
@@ -1,34 +1,100 @@
 package ui;
 
+import functions.TabulatedFunction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
-
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class WebController {
 
+    @Autowired
+    private FunctionService functionService;
+
+    @Autowired
+    private ParsingService parsingService;
+
     @RequestMapping("/")
-    public String showHomePage(Model model){
+    public String showHomePage(){
         return "homePage";
     }
 
     @PostMapping("/setSettings")
     public void getSettings(@RequestParam("functionSelect") String type){
-        System.out.println(type);
+        functionService.setSetting(type);
     }
 
-    @PostMapping("/createMathFunction")
-    public void createMathFunction(@RequestParam("functionSelect") String function, @RequestParam("pointCount") int count,
+    @PostMapping("/serializeMathFunction")
+    @ResponseBody
+    public String serializeMathFunction(@RequestParam("functionSelect") String function, @RequestParam("pointCount") int count,
                                    @RequestParam("xFrom") double xFrom, @RequestParam("xTo") double xTo){
-        System.out.println(function);
-        System.out.println(count);
-        System.out.println(xFrom);
-        System.out.println(xTo);
-
+        return functionService.serializeMathFunction(function,count,xFrom,xTo);
     }
+
+    @PostMapping("/serializeTabulatedFunction")
+    @ResponseBody
+    public String serializeTabulatedFunction(@RequestParam("X") String x,@RequestParam("Y") String y){
+        return functionService.serializeTabulatedFunction(parsingService.parseStringToDoubleArray(x),parsingService.parseStringToDoubleArray(y));
+    }
+
+    @PostMapping("/differentiateMathFunction")
+    @ResponseBody
+    public String differentiateMathFunction(@RequestParam("functionSelect") String function, @RequestParam("pointCount") int count,
+                                   @RequestParam("xFrom") double xFrom, @RequestParam("xTo") double xTo){
+        TabulatedFunction tabulatedFunction1 = functionService.createTabulatedFunction(function,count,xFrom,xTo);
+        TabulatedFunction tabulatedFunction2 = functionService.differentiateMathFunction(function,count,xFrom,xTo);
+        return parsingService.parseTwoTabulatedFunctionToString(tabulatedFunction1,tabulatedFunction2);
+    }
+
+    @PostMapping("/differentiateFunction")
+    @ResponseBody
+    public String differentiateFunction(@RequestParam("X") String x,@RequestParam("Y") String y){
+        TabulatedFunction func = functionService.differentiateFunction(parsingService.parseStringToDoubleArray(x),parsingService.parseStringToDoubleArray(y));
+        return parsingService.parseTabulatedFunctionToString(func);
+    }
+
+    @PostMapping("/calculateOperationResult")
+    @ResponseBody
+    public String calculateOperationResult(@RequestParam("operationType") String type,
+                                           @RequestParam("firstOperand") String firstOperand, @RequestParam("secondOperand") String secondOperand){
+        double [] firstX = parsingService.parseStringToDoubleArray(parsingService.getFirstPartOfCombinedString(firstOperand));
+        double [] firstY = parsingService.parseStringToDoubleArray(parsingService.getSecondPartOfCombinedString(firstOperand));
+        TabulatedFunction firstTF = functionService.createTabulatedFunction(firstX,firstY);
+        double [] secondX = parsingService.parseStringToDoubleArray(parsingService.getFirstPartOfCombinedString(secondOperand));
+        double [] secondY = parsingService.parseStringToDoubleArray(parsingService.getSecondPartOfCombinedString(secondOperand));
+        TabulatedFunction secondTF = functionService.createTabulatedFunction(secondX,secondY);
+        return parsingService.parseTabulatedFunctionToString(functionService.calculateOperation(type,firstTF,secondTF));
+    }
+    @PostMapping("/serializeOperationResult")
+    @ResponseBody
+    public String serializeOperationResult(@RequestParam("X") String x,@RequestParam("Y") String y){
+        return functionService.serializeTabulatedFunction(parsingService.parseStringToDoubleArray(x),parsingService.parseStringToDoubleArray(y));
+    }
+
+    @PostMapping("/getMathFunction")
+    @ResponseBody
+    public String getMathFunction(@RequestParam("functionSelect") String function, @RequestParam("pointCount") int count,
+                                  @RequestParam("xFrom") double xFrom, @RequestParam("xTo") double xTo){
+        TabulatedFunction tabulatedFunction = functionService.createTabulatedFunction(function,count,xFrom,xTo);
+        return parsingService.parseTabulatedFunctionToString(tabulatedFunction);
+    }
+
+
+    @PostMapping("/calculateMathOperationResult")
+    @ResponseBody
+    public String calculateMathOperationResult(@RequestParam("operationType") String type,
+                                  @RequestParam("firstOperand") String firstOperand,@RequestParam("functionSelect") String function, @RequestParam("pointCount") int count,
+                                  @RequestParam("xFrom") double xFrom, @RequestParam("xTo") double xTo){
+        double [] firstX = parsingService.parseStringToDoubleArray(parsingService.getFirstPartOfCombinedString(firstOperand));
+        double [] firstY = parsingService.parseStringToDoubleArray(parsingService.getSecondPartOfCombinedString(firstOperand));
+        TabulatedFunction firstTF = functionService.createTabulatedFunction(firstX,firstY);
+        TabulatedFunction secondTF = functionService.createTabulatedFunction(function,count,xFrom,xTo);
+        return parsingService.parseTwoTabulatedFunctionToString(secondTF,functionService.calculateOperation(type,firstTF,secondTF));
+    }
+
+
+
+
+
 }
